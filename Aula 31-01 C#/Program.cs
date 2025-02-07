46812fd3e9e6e857652787feedd56891d26bd42c
Otaviolsv <143139884+Otaviolsv@users.noreply.github.com>
2025-02-07 09:24:14 -0300

    Add files via upload

---
+++ Aula 31-01 C#/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+string nome = "Otávio";
+
+Console.WriteLine("Olá " + nome);
+
+Console.WriteLine("Digite o primeiro número: ");
+int num1;
+    
+ num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("O número escolhido foi: " + num1);
+
+Console.ReadLine();
