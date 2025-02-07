8dd029a01abc99957ec0c561f9dfafba9c587379
Otaviolsv <143139884+Otaviolsv@users.noreply.github.com>
2025-02-07 09:24:57 -0300

    Add files via upload

---
+++ aula 07-02 C#/Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nomecompleto = "Otávio Vasconcelos";
+        string sobrenome;
+        string sobrenome2;
+
+        Console.WriteLine(nomecompleto);
+
+        Console.WriteLine("Nome em maiúsculo:" + nomecompleto.ToUpper());
+        // converte para letras maiusculas 
+
+        Console.WriteLine("Nome em minúsculo:" + nomecompleto.ToLower());
+        // converte para letras minusculas
+
+        Console.WriteLine(nomecompleto.Substring(0, nomecompleto.IndexOf(" ")));
+        // chama a string até a primeira ocorrencia definida
+
+        sobrenome = nomecompleto.Substring(nomecompleto.IndexOf(" ") +1);
+        sobrenome2 = nomecompleto.Substring(7, 11);
+
+
+        Console.WriteLine(sobrenome);
+        Console.WriteLine(sobrenome2);
+
+        string[] array = nomecompleto.Split('2');
+
+        Console.ReadLine();
+
+    }
+}
