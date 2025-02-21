ceec9ad11de85b36274a4fdf5bbad2e412332543
Otaviolsv <143139884+Otaviolsv@users.noreply.github.com>
2025-02-21 09:25:50 -0300

    Add files via upload
    Ex 6 , 7 e 11

---
+++ Aula 21-02 C#/Program.cs
@@ -0,0 +1,101 @@
+﻿//Exercício 6 - Escreva um programa que solicite peso e altura do usuário e calcule seu Índice de Massa Corporal (IMC), classificando-o em faixas de peso. 
+
+//Observações:
+
+//Utilize a fórmula: IMC = peso / (altura * altura).
+//Classifique o usuário conforme as faixas do IMC.
+//Considerações:
+
+//Cálculo correto do IMC.
+//Exibição da classificação correta.
+
+
+//Console.WriteLine("============EX6=============");
+//Console.WriteLine("\nInforme o seu peso:");
+
+//double peso = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Informe a sua altura");
+
+//double altura = Convert.ToDouble(Console.ReadLine());
+
+//double imc = (peso / (altura * altura)) * 10000;
+
+//Console.WriteLine($"\nO seu IMC é {imc}");
+
+//if(imc < 17)
+//{
+//    Console.WriteLine("Você está muito abaixo do peso");
+//}
+//else if(imc <= 18.49)
+//{
+//    Console.WriteLine("Você está abaixo do peso");
+//}
+//else if(imc <= 24.99)
+//{
+//    Console.WriteLine("Você está no peso ideal");
+//}
+//else if(imc <= 29.99)
+//{
+//    Console.WriteLine("Você está acima do peso");
+//}
+//else if(imc <= 34.99)
+//{
+//    Console.WriteLine("Você está com obesidade");
+//}
+//else if(imc <= 39.99)
+//{
+//    Console.WriteLine("Você está com obesidade severa");
+//}
+//else if(imc >= 40)
+//{
+//    Console.WriteLine("Você está com obesidade mórbida");
+//}
+
+
+
+//* Exercício 7 - Verificador de Número Par ou Ímpar 
+
+//Crie um programa que solicite um número inteiro e determine se ele é par ou ímpar.
+
+//Observações:
+
+//Utilize estruturas condicionais para verificar a paridade.
+//Exiba uma mensagem indicando o resultado.
+//Considerações:
+
+//Uso correto de estruturas condicionais.
+//Cálculo correto da paridade.
+
+
+//Console.WriteLine("\n============EX7=============");
+//Console.WriteLine("\nDigite um número:");
+
+//int numero = Convert.ToInt32(Console.ReadLine());
+
+//if(numero % 2 == 0)
+//{
+//    Console.WriteLine($"\nO número {numero} é par");
+//}
+
+//else
+//{
+//    Console.WriteLine($"\nO número {numero} é impar");
+//}
+
+
+//Exercício 11 Tabuada Interativa
+
+//Desenvolva um programa que solicite um número ao usuário e exiba sua tabuada de 1 a 10. 
+
+//Observações:
+
+//Utilize um laço de repetição para calcular a tabuada.
+//Considerações:
+
+//Uso adequado do laço de repetição.
+//Cálculo correto da tabuada.
+
+Console.WriteLine("\n============EX11=============");
+
+Console.WriteLine("");
