cccd850fb02d75f1890600ac084ecb1bad45fec1
Otaviolsv <143139884+Otaviolsv@users.noreply.github.com>
2025-03-12 09:25:46 -0300

    Add files via upload

---
+++ Produto/Produto/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//Crie uma classe Produto com atributos como nome, preço e quantidade em estoque.
+//Implemente um constructor e metodos para  adicionar e remover itens do estoque.
+
+class Produto
+{
+    public String nome;
+    double preço;
+    int quantidade;
+    public Produto()
+    {
+        nome = "maça";
+        preço = 5.0;
+        quantidade = 10;
+    }
+}
